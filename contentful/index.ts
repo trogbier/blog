1962185633f32861df6326f495193e92820b31da
Alex Rotar <alexrotar222@gmail.com>
2021-09-23 15:36:54 +0300

    join cms

---
+++ contentful/index.ts
@@ -0,0 +1,8 @@
+import {createClient} from 'contentful'
+
+const client = createClient({
+    space: '4twfyxn0h4ow',
+    accessToken: 'prvn25EfXVPOYDdIBN_poW8e6OFhkCrbMWBytCF9aMQ'
+})
+
+export default client
